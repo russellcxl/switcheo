774cf16d0822b70bab9fb57222016def8581fa45
Russell Chan <russell.chanxl@shopee.com>
2022-07-07 21:21:08 +0800

    update truffle config

--- src/problem5/test/test.js
+++ src/problem5/test/test.js
@@ -1,28 +1,35 @@
 const ethers = require("ethers");
 const { abi } = require("../build/contracts/Reader.json");
+require('dotenv').config();
 
-const ADDR = "0x4ce99954673B7026F6C560FED9eBde64ecFB8c90";   // your contract address
+
+const ADDR = "0x6B175474E89094C44Da98b954EedeAC495271d0F";   // your contract address
 const ABI = [abi];    // your contract ABI
 
-const ADDRESS = "0x13F3bB32Fe57bF4c4434b2cF426995044123EAE4"; // some wallet address with token balance
-const TOKENS = [];
+const ADDRESS = "0x6c6Bc977E13Df9b0de53b251522280BB72383700"; // some wallet address with token balance
+const TOKENS = [
+    "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359"
+];
 
 // you can use your own RPC provider url (no need to deploy to mainnet)
-const provider = ethers.providers.getDefaultProvider('kovan');
+// const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_ID}`);
+const provider = new ethers.providers.getDefaultProvider('ropsten')
 
-const test = async () => {
+const main = async () => {
 	const contract = new ethers.Contract(ADDR, ABI, provider);
-
 	const balances = await contract.getBalances(ADDRESS, TOKENS);
 
     // return in expected struct
-	return balances.map(bal => {
+	balances.map(bal => {
         return {
             token: bal.token,
             balance: bal.balance
         }
     });
 
+	console.log(balances)
 };
 
-test().then(console.log);
+main()
--- src/problem5/truffle-config.js
+++ src/problem5/truffle-config.js
@@ -1,25 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    },
-    develop: {
-      port: 8545
-    },
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          `https://kovan.infura.io/${process.env.INFURA_API_KEY}`
-        )
-      },
-      gas: 5000000,
-      gasPrice: 25000000000,
-      network_id: 42
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // for more about customizing your Truffle configuration!
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*" // Match any network id
+        },
+        develop: {
+            port: 8545
+        },
+        ropsten: {
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`),
+            network_id: 3,       // Ropsten's id
+            gas: 5500000,        // Ropsten has a lower block limit than mainnet
+            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        },
     }
-  }
 };
